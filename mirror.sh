400af2804e3f7cb94fb69dd024c4f5b5cbd49d03
Cameron <csanders@protonmail.com>
2020-07-29 14:58:57 -0500

    fix typos. fix credentials passing

--- mirror.sh
+++ mirror.sh
@@ -7,22 +7,39 @@ function conf_prop {
   cat $conf_file | grep -i "$prop=" | cut -d"=" -f2
 }
 
-function get_repos {
+function get_repo_urls {
   local base="https://api.github.com"
   local tail="repos?per_page=100"
   local type=$1
   local name=$2
-  local cred=$2
+  local cred=$3
   local url="${base}/${type}/${name}/${tail}"
 
   local curl_args="-s"
 
-  if [[ $type == "users" && ! -z "$cred" ]]
+  if [[ -n "$cred" ]]
   then
-    $curl_args="$curl_args -u \"$cred\""
+    curl_args="$curl_args -u $cred"
   fi
 
   curl $curl_args "$url" | grep "clone_url" | cut -d'"' -f4
+}
+
+function mirror_repos {
+  local repos=$@
+  for repo in $repos
+  do
+    project=`basename "$repo"`
+    echo "Mirroring $project"
+    if [ -d "$project" ]; 
+    then
+      cd "$project"
+      git fetch --prune
+      cd ..
+    else
+      git clone --mirror "$repo" "$project"
+    fi
+  done
 }
 
 if [[ ! -e "$conf_file" ]]
@@ -51,18 +68,5 @@ then
    exit 1
 fi
 
-repos=`get_repos $TYPE $NAME $CRED`
-
-for repo in $repos
-do
-  project=`basename "$repo"`
-  echo "Mirroring $project"
-  if [ -d "$project" ]; 
-  then
-    cd "$project"
-    git fetch --prune
-    cd ..
-  else
-    git clone --mirror "$repo" "$project"
-  fi
-done
+repos=`get_repo_urls "$TYPE" "$NAME" "$CRED"`
+mirror_repos $repos
