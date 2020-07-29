1a39547730810ae867795eb70eea8ed73bee3ad4
Cameron <csanders@protonmail.com>
2020-07-29 17:21:45 -0500

    fix multipage clone. fix clone private repos over ssh

--- mirror.sh
+++ mirror.sh
@@ -8,21 +8,38 @@ function conf_prop {
 }
 
 function get_repo_urls {
-  local base="https://api.github.com"
-  local tail="repos?per_page=100"
+  local tmp=`mktemp`
+
   local type=$1
   local name=$2
   local cred=$3
-  local url="${base}/${type}/${name}/${tail}"
+  
+  local clone_from="clone_url"
+
+  local url_base="https://api.github.com"
+  local url_tail="repos?per_page=100"
+  local url="${url_base}/${type}/${name}/${url_tail}"
 
   local curl_args="-s"
 
   if [[ -n "$cred" ]]
   then
     curl_args="$curl_args -u $cred"
+
+    if [[ "$type" == "users" ]]
+    then
+      clone_from="ssh_url"
+      url="${url_base}/user/${url_tail}"
+    fi
   fi
 
-  curl $curl_args "$url" | grep "clone_url" | cut -d'"' -f4
+  while [ -n "$url" ]; do
+    curl $curl_args --dump-header "$tmp" "$url"
+    url="`< "$tmp" grep Link | grep -oE "[a-zA-Z0-9:/?=.&_]*>; rel=.next" | cut -d'>' -f1`"
+  done | grep "$clone_from" | cut -d'"' -f4 | while read url;
+  do
+    echo $url
+  done
 }
 
 function mirror_repos {
